d861918f19f41c1f868a5052e7587c59aaa2953b
OtavioFilipe <darksnack16@gmail.com>
2023-12-01 17:00:07 -0300

    add css colors

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 function CardGame() {
     return `
         <article class="card-game">
-            CardGame
+            <img src="images/alura-pixel.png" alt="Logo da Alura">
         </article>
     `
 }
