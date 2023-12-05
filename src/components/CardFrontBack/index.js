3d4b2d53b1db18d3b04ee2abafdd768802d703a9
OtavioFilipe <darksnack16@gmail.com>
2023-12-05 17:18:02 -0300

    introduces a new feature flip card

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,10 +1,15 @@
+import "./style.css";
 import CardGame from "../CardGame";
 
 function CardFrontBack() {
   return `
         <article class="card-front-back">
+        <div class="card -front">
             ${CardGame()}
+        </div>
+        <div class="card -back">
             ${CardGame("javaScript-pixel", "Logo do JavaSript")}
+        </div>
         </article>
     `;
 }
