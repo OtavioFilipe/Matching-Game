3690ff4a4bcaba15ccefaa11df6e9050a2becaa3
OtavioFilipe <darksnack16@gmail.com>
2023-12-06 16:24:41 -0300

    fix implement

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,11 +1,17 @@
 import "./style.css";
 import PlayerName from "../../components/PlayeName";
+import PlayerScore from "../../components/PlayerScore";
+import VsPlayer from "../../components/VsPlayer";
 
 function ScoreBoard() {
   return `
         <header class="score-board">
         ${PlayerName("Player 1")}
+        ${PlayerScore()}
+        ${VsPlayer()}
+        ${PlayerScore()}
         ${PlayerName("Player 2")}
+
         </header>
     `;
 }
