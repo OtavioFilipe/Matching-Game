ef8223f7e5cf74187bb6e047d5cfdc7e6d67053c
OtavioFilipe <darksnack16@gmail.com>
2023-12-06 16:23:41 -0300

    create score count

---
+++ src/components/PlayerScore/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+function PlayerScore() {
+  return `
+        <div class="score-box">
+            <div class="score-count"></div>
+            <div class="score-count"></div>
+            <div class="score-count"></div>
+        </div>
+    
+    `;
+}
+
+export default PlayerScore;
