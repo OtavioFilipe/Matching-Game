c25fd6ccf26d2fe09b843e57264bfa867cbfb246
OtavioFilipe <darksnack16@gmail.com>
2023-12-07 17:50:58 -0300

    create the javascript, html and css cards

---
+++ src/objects/BoardGame/data.js
@@ -0,0 +1,28 @@
+const cards = [
+  {
+    icon: "html-pixel",
+    altIcon: "Html Logo",
+  },
+  {
+    icon: "html-pixel",
+    altIcon: "Html Logo",
+  },
+  {
+    icon: "css-pixel",
+    altIcon: "Css Logo",
+  },
+  {
+    icon: "css-pixel",
+    altIcon: "Css Logo",
+  },
+  {
+    icon: "javaScript-pixel",
+    altIcon: "JavaScript Logo",
+  },
+  {
+    icon: "javaScript-pixel",
+    altIcon: "JavaScript Logo",
+  },
+];
+
+export default cards;
